cac8f43c65c742039f89a7ba7835468562ff89a6
emer <ershad.qurishy7@gmail.com>
2024-02-21 18:49:48 +0300

    full code
    her take this simple code for your use it is written in C++.

---
+++ Program.cpp
@@ -0,0 +1,46 @@
+
+#include "LoginForm.h"
+#include "MainForm.h"
+#include "RegisterForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	User^ user = nullptr;
+	while (true) {
+		MyRestaurantProject::LoginForm loginForm;
+		loginForm.ShowDialog();
+		
+		if (loginForm.switchToRegister) {
+			//show the register form
+			MyRestaurantProject::RegisterForm registerForm;
+			registerForm.ShowDialog();
+
+			if (registerForm.switchToLogin) {
+				continue;
+			}
+			else {
+				user = registerForm.user;
+				break;
+			}
+		}
+		else {
+			user = loginForm.user;
+			break;
+		}
+	}
+
+	if (user != nullptr) {
+		MyRestaurantProject::MainForm mainForm(user);
+		Application::Run(% mainForm);
+	}
+	else {
+		MessageBox::Show("Authentication Canceled",
+			"Program.cpp", MessageBoxButtons::OK);
+	}
+}
